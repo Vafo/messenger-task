d58efb8b5d377a4e2cd6061dc840ac12e950305e
vafo <v.safokhujaev@student.inha.uz>
2023-09-12 19:05:43 +0500

    moved details of make_buff

--- messenger.cpp
+++ messenger.cpp
@@ -90,73 +90,32 @@ public:
 
 };
 
+// Push sequentially single packet from stream of text 
+std::string::const_iterator push_single_packet (
+    const std::string &name, 
+    std::string::const_iterator msg_begin, 
+    std::string::const_iterator msg_end, 
+    std::vector<uint8_t> &out_vec
+);
+
 } // namespace detail
 
 std::vector<uint8_t> make_buff(const msg_t & msg) {
-    if(msg.name.empty()) {
-        throw std::length_error("Message name is empty");
-    }
+    if(msg.name.empty()) throw std::length_error("messenger: sender's name is empty");
     
-    if(msg.text.empty()) {
-        throw std::length_error("Message text is empty");
-    }
+    if(msg.text.empty()) throw std::length_error("messenger: text is empty");
     
-    if(msg.name.size() >= MSGR_NAME_LEN_MAX) {
-        throw std::length_error("Message name length exceeds maximum length " DEF_TO_STR(MSGR_NAME_LEN_MAX) );
-    }
+    if(msg.name.size() >= MSGR_NAME_LEN_MAX)
+        throw std::length_error("messenger: name length exceeds maximum length " DEF_TO_STR(MSGR_NAME_LEN_MAX) );
 
     // Vector to store result of func
     std::vector<uint8_t> res;
 
-    std::string::size_type msg_text_len = msg.text.size(); // Size of uncovered msg.text
-    std::string::size_type msg_text_offset = 0; // Beginning of packet's text within msg.text
-    std::string::size_type msg_text_offset_end = -1; // End of packet's text within msg.text
-    std::vector<uint8_t>::size_type header_offset = 0;
-    int packet_text_len = 0; // Length of text of cur packet. Either MAX or trailing of msg.text
+    std::string::const_iterator next_packet_pos = msg.text.begin();
+
     // Loop per packet
-    while(msg_text_len > 0) {
-        uint8_t crc4_res = 0;
-        packet_text_len = MIN(msg_text_len, MSGR_MSG_LEN_MAX);
-
-        // Header part of packet
-        detail::msg_hdr_t header(FLAG_BITS, msg.name.size(), packet_text_len);
-        crc4_res = header.calculate_crc4(); // Calculate crc4, masking crc4 bits with zeroes
-        // Store cur loc of header within vector and make space for it. Header will be placed later
-        header_offset = res.size();
-        res.resize(res.size() + sizeof(header));
-
-        // Sender (?) Name part of packet
-        for(std::string::size_type i = 0; i < msg.name.size(); i++) {
-            std::string::value_type val = msg.name[i];
-            crc4_res = util::crc4(crc4_res, val, BITS_PER_BYTE); // Calculate crc4 per byte of name
-            res.push_back( static_cast<uint8_t>(val) );
-        }
-        
-        // Text part of packet
-        for(msg_text_offset_end = msg_text_offset + packet_text_len ; 
-            msg_text_offset < msg_text_offset_end;
-            msg_text_offset++ ) {
-            std::string::value_type val = msg.text[msg_text_offset];
-            crc4_res = util::crc4(crc4_res, val, BITS_PER_BYTE); // Calculate crc4 per byte of text
-            res.push_back( static_cast<uint8_t>(val) );
-        }
-        
-        // Place header into packet with calculated crc4
-        header.set_crc4(crc4_res);
-
-        // Should endianness be really considered?
-        if(util::is_little_endian()) {
-            // Modify vector using pointers (not sure if there is other safer way to modify contiguous bytes)
-            std::copy(header.begin(), header.end(), res.begin() + header_offset);
-        } else {
-            // It does not flip endianness, it just changes start of copy procedure
-            // std::copy_backward(hdr_beg_ptr, hdr_end_ptr, res.begin() + header_offset);
-            throw std::runtime_error("Big endian conversion is required");
-        }
-
-        // Reduce size of uncovered text
-        msg_text_len -= packet_text_len;
-    }
+    while(next_packet_pos != msg.text.cend())
+        next_packet_pos = detail::push_single_packet(msg.name, next_packet_pos, msg.text.cend(), /* out */ res);
 
     return res;
 }
@@ -253,77 +212,128 @@ msg_t parse_buff(std::vector<uint8_t> & buff) {
 */
 namespace detail {
 
-    uint8_t *msg_hdr_t::begin() {
-        return reinterpret_cast<uint8_t *>(&this->m_hdr);
+uint8_t *msg_hdr_t::begin() {
+    return reinterpret_cast<uint8_t *>(&this->m_hdr);
+}
+
+uint8_t *msg_hdr_t::end() {
+    return reinterpret_cast<uint8_t *>(&this->m_hdr) + sizeof(this->m_hdr);
+}
+
+// Set flag bits of header
+inline void msg_hdr_t::set_flag(uint8_t flag) {
+    this->m_hdr[0] &= ~(MASK_FIRST_N(MSGR_FLAG_BITS)); // Remove prev value of flag bits;
+    this->m_hdr[0] |= flag & MASK_FIRST_N(MSGR_FLAG_BITS);
+}
+
+// Get flag bits of header
+inline uint8_t msg_hdr_t::get_flag() {
+    return this->m_hdr[0] & MASK_FIRST_N(MSGR_FLAG_BITS);
+}
+
+// Set name_len bits of header
+inline void msg_hdr_t::set_name_len(uint8_t name_len) {
+    this->m_hdr[0] &= ~( MASK_FIRST_N(MSGR_NAME_LEN_BITS) << MSGR_FLAG_BITS );  // Remove prev value of name_len bits
+    this->m_hdr[0] |= (name_len & MASK_FIRST_N(MSGR_NAME_LEN_BITS)) << MSGR_FLAG_BITS;
+}
+
+// Get name_len bits of header
+inline uint8_t msg_hdr_t::get_name_len() {
+    return (this->m_hdr[0] >> MSGR_FLAG_BITS) & MASK_FIRST_N(MSGR_NAME_LEN_BITS);
+}
+
+// Set msg_len bits of header
+inline void msg_hdr_t::set_msg_len(uint8_t msg_len) {
+    this->m_hdr[0] &= ~(1 << (MSGR_FLAG_BITS + MSGR_NAME_LEN_BITS)); // Remove 1st bit of msg_len in 0th byte
+    this->m_hdr[1] &= ~(MASK_FIRST_N(MSGR_MSG_LEN_BITS - 1 /* exclude 1 from tot */));
+    this->m_hdr[0] |= (msg_len & 1) << (MSGR_FLAG_BITS + MSGR_NAME_LEN_BITS);
+    this->m_hdr[1] |= (msg_len >> 1) & (MASK_FIRST_N(MSGR_MSG_LEN_BITS - 1));
+}
+
+// Get msg_len bits of header
+inline uint8_t msg_hdr_t::get_msg_len() {
+    uint8_t res = (this->m_hdr[0] >> (MSGR_FLAG_BITS + MSGR_NAME_LEN_BITS)) & 1;
+    return res | ((this->m_hdr[1] & MASK_FIRST_N(MSGR_MSG_LEN_BITS - 1)) << 1)  ;
+}
+
+// Set crc4 bits of header
+inline void msg_hdr_t::set_crc4(uint8_t crc4_val) {
+    this->m_hdr[1] &= ~(MASK_FIRST_N(MSGR_CRC4_BITS) << (MSGR_MSG_LEN_BITS - 1));
+    this->m_hdr[1] |= (crc4_val & MASK_FIRST_N(MSGR_CRC4_BITS)) << (MSGR_MSG_LEN_BITS - 1);
+}
+
+// Get crc4 bits of header
+inline uint8_t msg_hdr_t::get_crc4() {
+    return (this->m_hdr[1] >> (MSGR_MSG_LEN_BITS - 1)) & MASK_FIRST_N(MSGR_CRC4_BITS);
+}
+
+// Calculate crc4 of packet masking crc4 bits with zeroes (might consider revision)
+inline uint8_t msg_hdr_t::calculate_crc4() {
+    uint8_t res = 0;
+    // Not safe in case of concurrent access
+    uint8_t tmp_crc4 = this->get_crc4();
+    this->set_crc4(0);
+    uint8_t *beg = reinterpret_cast<uint8_t *>(this->m_hdr);
+    uint8_t *end = beg + sizeof(this->m_hdr);
+    while(beg < end) {
+        res = util::crc4(res, *beg++, BITS_PER_BYTE);
     }
 
-    uint8_t *msg_hdr_t::end() {
-        return reinterpret_cast<uint8_t *>(&this->m_hdr) + sizeof(this->m_hdr);
+    this->set_crc4(tmp_crc4);
+
+    return res;
+}
+
+std::string::const_iterator push_single_packet (
+    const std::string &name, 
+    std::string::const_iterator msg_begin, 
+    std::string::const_iterator msg_end, 
+    std::vector<uint8_t> &out_vec
+) {
+    // Length of message of packet
+    std::string::size_type packet_msg_len = MIN(msg_end - msg_begin, MSGR_MSG_LEN_MAX);
+    
+    // Header part of packet
+    msg_hdr_t header(FLAG_BITS, name.size(), packet_msg_len);
+    
+    uint8_t crc4_res = 0;
+    crc4_res = header.calculate_crc4(); // Calculate crc4, masking crc4 bits with zeroes
+    // Store cur loc of header within vector and make space for it. Header will be placed later
+    std::vector<uint8_t>::size_type header_offset = out_vec.size();
+    out_vec.resize(header_offset + sizeof(header));
+
+    // Calculate crc4 and push name part of packet
+    for(std::string::size_type i = 0; i < name.size(); i++) {
+        std::string::value_type val = name[i];
+        crc4_res = util::crc4(crc4_res, val, BITS_PER_BYTE); // Calculate crc4 per byte of name
+        out_vec.push_back( static_cast<uint8_t>(val) );
+    }
+    
+    // Text part of packet
+    for(std::string::const_iterator iter = msg_begin, end = msg_begin + packet_msg_len; 
+        iter != end;
+        iter++ ) {
+        std::string::value_type val = *iter;
+        crc4_res = util::crc4(crc4_res, val, BITS_PER_BYTE); // Calculate crc4 per byte of text
+        out_vec.push_back( static_cast<uint8_t>(val) );
+    }
+    
+    // Place header into packet with calculated crc4
+    header.set_crc4(crc4_res);
+
+    // Should endianness be really considered?
+    if(util::is_little_endian()) {
+        // Modify vector using pointers (not sure if there is other safer way to modify contiguous bytes)
+        std::copy(header.begin(), header.end(), out_vec.begin() + header_offset);
+    } else {
+        // It does not flip endianness, it just changes start of copy procedure
+        // std::copy_backward(hdr_beg_ptr, hdr_end_ptr, res.begin() + header_offset);
+        throw std::runtime_error("messenger: big endian conversion is required");
     }
 
-    // Set flag bits of header
-    inline void msg_hdr_t::set_flag(uint8_t flag) {
-        this->m_hdr[0] &= ~(MASK_FIRST_N(MSGR_FLAG_BITS)); // Remove prev value of flag bits;
-        this->m_hdr[0] |= flag & MASK_FIRST_N(MSGR_FLAG_BITS);
-    }
+    return msg_begin + packet_msg_len;
+}
 
-    // Get flag bits of header
-    inline uint8_t msg_hdr_t::get_flag() {
-        return this->m_hdr[0] & MASK_FIRST_N(MSGR_FLAG_BITS);
-    }
-
-    // Set name_len bits of header
-    inline void msg_hdr_t::set_name_len(uint8_t name_len) {
-        this->m_hdr[0] &= ~( MASK_FIRST_N(MSGR_NAME_LEN_BITS) << MSGR_FLAG_BITS );  // Remove prev value of name_len bits
-        this->m_hdr[0] |= (name_len & MASK_FIRST_N(MSGR_NAME_LEN_BITS)) << MSGR_FLAG_BITS;
-    }
-
-    // Get name_len bits of header
-    inline uint8_t msg_hdr_t::get_name_len() {
-        return (this->m_hdr[0] >> MSGR_FLAG_BITS) & MASK_FIRST_N(MSGR_NAME_LEN_BITS);
-    }
-
-    // Set msg_len bits of header
-    inline void msg_hdr_t::set_msg_len(uint8_t msg_len) {
-        this->m_hdr[0] &= ~(1 << (MSGR_FLAG_BITS + MSGR_NAME_LEN_BITS)); // Remove 1st bit of msg_len in 0th byte
-        this->m_hdr[1] &= ~(MASK_FIRST_N(MSGR_MSG_LEN_BITS - 1 /* exclude 1 from tot */));
-        this->m_hdr[0] |= (msg_len & 1) << (MSGR_FLAG_BITS + MSGR_NAME_LEN_BITS);
-        this->m_hdr[1] |= (msg_len >> 1) & (MASK_FIRST_N(MSGR_MSG_LEN_BITS - 1));
-    }
-
-    // Get msg_len bits of header
-    inline uint8_t msg_hdr_t::get_msg_len() {
-        uint8_t res = (this->m_hdr[0] >> (MSGR_FLAG_BITS + MSGR_NAME_LEN_BITS)) & 1;
-        return res | ((this->m_hdr[1] & MASK_FIRST_N(MSGR_MSG_LEN_BITS - 1)) << 1)  ;
-    }
-
-    // Set crc4 bits of header
-    inline void msg_hdr_t::set_crc4(uint8_t crc4_val) {
-        this->m_hdr[1] &= ~(MASK_FIRST_N(MSGR_CRC4_BITS) << (MSGR_MSG_LEN_BITS - 1));
-        this->m_hdr[1] |= (crc4_val & MASK_FIRST_N(MSGR_CRC4_BITS)) << (MSGR_MSG_LEN_BITS - 1);
-    }
-
-    // Get crc4 bits of header
-    inline uint8_t msg_hdr_t::get_crc4() {
-        return (this->m_hdr[1] >> (MSGR_MSG_LEN_BITS - 1)) & MASK_FIRST_N(MSGR_CRC4_BITS);
-    }
-
-    // Calculate crc4 of packet masking crc4 bits with zeroes (might consider revision)
-    inline uint8_t msg_hdr_t::calculate_crc4() {
-        uint8_t res = 0;
-        // Not safe in case of concurrent access
-        uint8_t tmp_crc4 = this->get_crc4();
-        this->set_crc4(0);
-        uint8_t *beg = reinterpret_cast<uint8_t *>(this->m_hdr);
-        uint8_t *end = beg + sizeof(this->m_hdr);
-        while(beg < end) {
-            res = util::crc4(res, *beg++, BITS_PER_BYTE);
-        }
-
-        this->set_crc4(tmp_crc4);
-
-        return res;
-    }
 
 } // namespace detail
 
