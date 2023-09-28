6736d532bd138b20513f3b0efb421f6b32f78bbe
vafo <v.safokhujaev@student.inha.uz>
2023-09-28 17:50:05 +0500

    separated initialization of hardcoded packets
    Related to #1

--- test/test_util.cpp
+++ test/test_util.cpp
@@ -31,4 +31,39 @@ std::vector<T> repeat_vector(const std::vector<T> &src, size_t times) {
 // Instantion for uint8_t
 template std::vector<uint8_t> repeat_vector<uint8_t>(const std::vector<uint8_t> &src, size_t times);
 
+std::vector<uint8_t> hardcoded_packet_max_text(
+    std::string &test_name,
+    std::string &test_text
+) {
+    test_name = "Name";
+    // Length 31 (NAME_LEN_MAX) (exclude \0 from count)
+    test_text = "ABCDEFGHIJKLMNOPQRSTUVWXY ?/.\'0";
+    std::vector<uint8_t> hardcoded_single_packet = {
+        0xa5, 0x6f /*header bytes FlAG:0x5 NAME_LEN:0x4 MSG_LEN:0x1f CRC4:0x6*/
+    };
+
+    packet_insert_iter hard_iter(hardcoded_single_packet);
+    hard_iter = std::copy(test_name.begin(), test_name.end(), hard_iter);
+    std::copy(test_text.begin(), test_text.end(), hard_iter);
+
+    return hardcoded_single_packet;
+}
+
+std::vector<uint8_t> hardcoded_packet_short_text(
+    std::string &test_name,
+    std::string &test_text
+) {
+    test_name = "Eman";
+    test_text = "Lorem ipsum";
+    std::vector<uint8_t> hardcoded_packet = {
+        0xa5, 0x85/*header bytes FlAG:0x5 NAME_LEN:0x4 MSG_LEN:0x0b CRC4:0x8*/
+    };
+    // Populate hardcoded packet
+    packet_insert_iter hard_iter(hardcoded_packet);
+    hard_iter = std::copy(test_name.begin(), test_name.end(), hard_iter);
+    std::copy(test_text.begin(), test_text.end(), hard_iter);
+
+    return hardcoded_packet;
+}
+
 } // namespace test::util
--- test/test_util.hpp
+++ test/test_util.hpp
@@ -8,10 +8,43 @@ namespace test {
 
 namespace util {
 
+using packet_insert_iter = std::back_insert_iterator<std::vector<uint8_t>>;
+
+
 std::string repeat_string(const std::string &src, size_t times);
 
 template<typename T>
 std::vector<T> repeat_vector(const std::vector<T> &src, size_t times);
+
+/**
+ * Hardcoded packet with max length text
+ * 
+ * @param test_name output packet's sender's name
+ * @param test_text output packet's message text
+ * 
+ * @returns hardcoded packet with message fields:
+ * Name: "Name"
+ * Text: "ABCDEFGHIJKLMNOPQRSTUVWXY ?/.\'0"
+*/
+std::vector<uint8_t> hardcoded_packet_max_text(
+    std::string &test_name/*out*/,
+    std::string &test_text/*out*/
+);
+
+/**
+ * Hardcoded packet with max length text
+ * 
+ * @param test_name output packet's sender's name
+ * @param test_text output packet's message text
+ * 
+ * @returns hardcoded packet with message fields:
+ * Name: "Eman"
+ * Text: "Lorem ipsum"
+*/
+std::vector<uint8_t> hardcoded_packet_short_text(
+    std::string &test_name/*out*/,
+    std::string &test_text/*out*/
+);
 
 } // namespace util
 
